fe22c4805dbc26d3704fd5a55d9b39b7c4dd76e1
Hackroid <ooozbh@gmail.com>
2017-12-09 03:54:54 +0800

    Spider test pass

--- src/tempUI/StartUp.java
+++ src/tempUI/StartUp.java
@@ -2,11 +2,13 @@
 
 package tempUI;
 
+import grab.grabEarthquake;
+
 public class StartUp {
 
 	public static void main(String[] args) {
 //		Main.start(args);
-		
+		grabEarthquake.generateURL();
 	}
 
 }
