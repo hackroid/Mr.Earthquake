de78e6de97981501d863907cb67610b09ca6dbac
sheepmanontheshore <30749942+sheepmanontheshore@users.noreply.github.com>
2017-12-11 22:48:13 +0800

    remove main

--- src/tempUI/Main.java
+++ src/tempUI/Main.java
@@ -154,10 +154,6 @@ public class Main extends Application {
          **/
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage stage) {
         stage.setTitle("Display EarthQuakes");
