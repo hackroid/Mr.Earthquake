14d0dc0344309d72e306bf23dbd410ff53cabe94
chenminken <zbcmb@outlook.com>
2017-12-25 20:48:13 +0800

    完善transformUtil工具类的功能

--- src/tempUI/TransformUtil.java
+++ src/tempUI/TransformUtil.java
@@ -9,10 +9,13 @@ import java.sql.SQLException;
 
 /**
  * Created by chenmin on 2017/12/25.
+ * have not been tested
  */
 public class TransformUtil {
-    public static ObservableList<Earthquake> RsToEq(ResultSet rs) {
+    public static ObservableList<Earthquake> SearchRequest(String UTC_date_start, String UTC_date_end, double magS, double magX, String region) {
         ObservableList<Earthquake> earthquakes = FXCollections.observableArrayList();
+        input data = new input(UTC_date_start, UTC_date_end, magS, magX, region);
+        ResultSet rs = data.search();
         try {
             while (rs.next()) {
                 earthquakes.add(new Earthquake(rs.getString(0),
@@ -29,9 +32,23 @@ public class TransformUtil {
         }
         return earthquakes;
     }
-    public static ResultSet SearchRequest(String UTC_date_start, String UTC_date_end, double magS, double magX, String region){
-        input data = new input(UTC_date_start, UTC_date_end, magS, magX, region);
-        ResultSet rs = data.search();
-        return rs;
-    }
+//      list all region
+//    public static ObservableList<Earthquake> RegionList(ResultSet rs) {
+//        ObservableList<Earthquake> earthquakes = FXCollections.observableArrayList();
+//        try {
+//            while (rs.next()) {
+//                earthquakes.add(new Earthquake(rs.getString(0),
+//                        rs.getString(1).replace("\"",""),
+//                        rs.getDouble(2),
+//                        rs.getDouble(3),
+//                        rs.getDouble(4),
+//                        rs.getDouble(5),
+//                        ""));
+//
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return earthquakes;
+//    }
 }
