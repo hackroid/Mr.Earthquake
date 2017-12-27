043e885d17c99fd317f50b86acfa184b72208622
chenminken <zbcmb@outlook.com>
2017-12-27 22:46:02 +0800

    update

--- src/search/getUniqueRegion.java
+++ src/search/getUniqueRegion.java
@@ -8,7 +8,7 @@ import java.sql.*;
 
 /**
  * get unique region list.
- * transform result set to observablelist<String></String>
+ * transform result set to observablelist<String>
  */
 public class getUniqueRegion {
 	/**
